d31dd3a5dd4c6d06ffd0cf97d4e36cb033999a10
huggcloud <38726983+huggcloud@users.noreply.github.com>
2018-05-15 00:23:10 +0200

    Create hugg.js

---
+++ hugg.js
@@ -0,0 +1,32 @@
+'use strict'
+
+document [`addEventListener`] (
+`DOMContentLoaded`
+
+, () => {
+/*  if (! (`WebSocket` in window)) {
+    console [`log`] (`websockets disabled`)
+    return
+  }
+
+  const websocket = new WebSocket (`wss://www.hugg.cloud:4343`)
+  websocket [`addEventListener`] (`message`, message)
+
+  function message (message_container) {
+    const message = JSON [`parse`] (message_container [`data`])
+
+    switch (message [`s`]) {
+      case ``:
+      break
+
+      default:
+    }
+  }*/
+}
+)
+
+/*window [`addEventListener`] (
+`error`
+,
+log
+)*/
